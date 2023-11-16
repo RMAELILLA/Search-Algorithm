9111649145d1e7987a4db59bb54b7da32fe6352d
RMAELILLA <ronioryzamae12@gmail.com>
2023-11-16 16:26:18 +0800

    change functon in app.py

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
 import timeit
 from flask import Flask, request, jsonify, render_template
-from exponential_search import exponential_search, exponential_search_wrapper
-from binary_search import binary_search, binary_search_wrapper
-from interpolation_search import interpolation_search, interpolation_search_wrapper
-from jump_search import jump_search, jump_search_wrapper
-from linear_search import linear_search, linear_search_wrapper
-from ternary_search import ternary_search, ternary_search_wrapper
-
+from linear_search import linear_search
+from binary_search import binary_search
+from exponential_search import exponential_search
+from jump_search import jump_search
+from interpolation_search import interpolation_search
+from ternary_search import ternary_search
 
 app = Flask(__name__)
 
-
-
 @app.route("/", methods=["GET", "POST"])
 def index():
-    
     numbers = range(1, 1001)
     test_data = ", ".join(map(str, numbers))
-    #print(test_data)
+
     if request.method == "POST":
         array_str = request.form.get("array")
         target_str = request.form.get("target")
@@ -28,41 +24,32 @@ def index():
             target = int(target_str)
             low, high = 0, len(array) - 1
 
-            result = -1  # Initialize result before the conditional statements
+            result = -1
 
             if search_type == "exponential":
-                execution_time = timeit.timeit("exponential_search_wrapper(exponential_search, array, target)", globals={**globals(), "array": array, "target": target}, number=1)  * 1000 
-                result = exponential_search_wrapper(binary_search, array, target)
-                # result = exponential_search(array, target)
+                execution_time = timeit.timeit("exponential_search(array, target)", globals={**globals(), "array": array, "target": target}, number=1) * 1000
+                result = exponential_search(array, target)
             elif search_type == "binary":
-                #arr = list(map(int, array_str.split(",")))
-                execution_time = timeit.timeit("binary_search_wrapper(binary_search, array, target)", globals={**globals(), "array": array, "target": target}, number=1)  * 1000 
-                result = binary_search_wrapper(binary_search, array, target)
+                execution_time = timeit.timeit("binary_search(array, target, low, high)", globals={**globals(), "array": array, "target": target, "low": low, "high": high}, number=1) * 1000
+                result = binary_search(array, target, low, high)
             elif search_type == "interpolation":
-                execution_time = timeit.timeit("interpolation_search_wrapper(interpolation_search, array, target)", globals={**globals(), "array": array, "target": target}, number=1)  * 1000 
-                result = interpolation_search_wrapper(interpolation_search, array, target)                
-                # result = interpolation_search(array, target)
+                execution_time = timeit.timeit("interpolation_search(array, target)", globals={**globals(), "array": array, "target": target}, number=1) * 1000
+                result = interpolation_search(array, target)
             elif search_type == "jump":
-                execution_time = timeit.timeit("jump_search_wrapper(jump_search, array, target)", globals={**globals(), "array": array, "target": target}, number=1)  * 1000 
-                result = jump_search_wrapper(interpolation_search, array, target)  
-                # result = jump_search(array, target)
+                execution_time = timeit.timeit("jump_search(array, target)", globals={**globals(), "array": array, "target": target}, number=1) * 1000
+                result = jump_search(array, target)
             elif search_type == "linear":
-                execution_time = timeit.timeit("linear_search_wrapper(linear_search, array, target)", globals={**globals(), "array": array, "target": target}, number=1)  * 1000 
-                result = linear_search_wrapper(linear_search, array, target)  
-                # result = linear_search(array, target)
+                execution_time = timeit.timeit("linear_search(array, target)", globals={**globals(), "array": array, "target": target}, number=1) * 1000
+                result = linear_search(array, target)
             elif search_type == "ternary":
-                execution_time = timeit.timeit("ternary_search_wrapper(ternary_search, array, target, low, high)", globals={**globals(), "array": array, "target": target,"low":low,"high":high}, number=1)  * 1000 
-                result = ternary_search_wrapper(ternary_search, array, target, low, high)  
-                # result = ternary_search(array, target, low, high)
+                execution_time = timeit.timeit("ternary_search(array, target)", globals={**globals(), "array": array, "target": target}, number=1) * 1000
+                result = ternary_search(array, target)
 
-            return render_template("index.html", result=result, search_type=search_type, execution_time=execution_time,test_data=test_data)
+            return render_template("index.html", result=result, search_type=search_type, execution_time=execution_time, test_data=test_data)
         except ValueError:
             return render_template("index.html", error="Invalid input. Ensure the array and target are integers.")
-    
 
-    return render_template("index.html",test_data=test_data)
-
-
+    return render_template("index.html", test_data=test_data)
 
 @app.route("/search", methods=["POST"])
 def search():
@@ -75,11 +62,9 @@ def search():
     target = data["target"]
 
     result_iterative = exponential_search(array, target)
-    #result_recursive = exponential_search_recursive(array, target)
 
     return jsonify({
         "iterative_search_result": result_iterative,
-       # "recursive_search_result": result_recursive
     })
 
 if __name__ == "__main__":
