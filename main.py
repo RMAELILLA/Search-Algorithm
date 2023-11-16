63d7db23269c0ad223d7402aaaaff5d7cf6e0f78
RMAELILLA <ronioryzamae12@gmail.com>
2023-11-16 15:25:04 +0800

    update main/py for better working code

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ def linear_search(arr, target):
             return time.time() - start_time
     return -1
 
-def binary_search(arr, target):
+def binary_search(arr, target, low, high):
     start_time = time.time()
-    low, high = 0, len(arr) - 1
     while low <= high:
         mid = (low + high) // 2
         if arr[mid] == target:
@@ -25,12 +24,11 @@ def exponential_search(arr, target):
     start_time = time.time()
     if arr[0] == target:
         return time.time() - start_time
-        return 0
     i = 1
     n = len(arr)
-    while i < n and arr [1] <= target:
+    while i < n and arr[i] <= target:
         i *= 2
-    return binary_search(arr, target, i // 2, min(i, n) - 1)
+    return binary_search(arr, target, 0, min(i, n) - 1)
 
 def jump_search(arr, target):
     start_time = time.time()
@@ -46,10 +44,45 @@ def jump_search(arr, target):
         if arr[i] == target:
             return time.time() - start_time
             return i
-        
+
     return -1
 
-# Add other search algorithms (Interpolation, Ternary) with similar structure
+def interpolation_search(arr, target):
+    start_time = time.time()
+    low, high = 0, len(arr) - 1
+
+    while low <= high and arr[low] <= target <= arr[high]:
+        pos = low + ((target - arr[low]) * (high - low) // (arr[high] - arr[low]))
+
+        if arr[pos] == target:
+            return time.time() - start_time
+        elif arr[pos] < target:
+            low = pos + 1
+        else:
+            high = pos - 1
+
+    return -1
+
+def ternary_search(arr, target):
+    start_time = time.time()
+    low, high = 0, len(arr) - 1
+
+    while low <= high:
+        mid1 = low + (high - low) // 3
+        mid2 = high - (high - low) // 3
+
+        if arr[mid1] == target:
+            return time.time() - start_time
+        elif arr[mid2] == target:
+            return time.time() - start_time
+        elif arr[mid1] < target:
+            low = mid1 + 1
+        elif arr[mid2] > target:
+            high = mid2 - 1
+        else:
+            low, high = mid1 + 1, mid2 - 1
+
+    return -1
 
 def generate_random_array(size):
     return sorted([random.randint(1, size * 10) for _ in range(size)])
@@ -62,18 +95,19 @@ def main():
         data_set = generate_random_array(size)
 
         linear_time = linear_search(data_set, target)
-        binary_time = binary_search(data_set, target)
+        binary_time = binary_search(data_set, target, 0, len(data_set) - 1)
         exponential_time = exponential_search(data_set, target)
         jump_time = jump_search(data_set, target)
-        # Add timing measurements for other search algorithms
+        interpolation_time = interpolation_search(data_set, target)
+        ternary_time = ternary_search(data_set, target)
 
         print(f"Data Size: {size}")
         print(f"Linear Search Time: {linear_time} seconds")
         print(f"Binary Search Time: {binary_time} seconds")
         print(f"Exponential Search Time: {exponential_time} seconds")
         print(f"Jump Search Time: {jump_time} seconds")
-        # Print timing measurements for other search algorithms
+        print(f"Interpolation Search Time: {interpolation_time} seconds")
+        print(f"Ternary Search Time: {ternary_time} seconds")
 
 if __name__ == "__main__":
     main()
-
