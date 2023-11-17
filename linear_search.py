cb7d3d44872147b29ba7ed21d511adf7d02556e8
RMAELILLA <ronioryzamae12@gmail.com>
2023-11-17 11:50:44 +0800

    display index linear but has (index, 0.0)

--- linear_search.py
+++ linear_search.py
@@ -4,5 +4,6 @@ def linear_search(arr, target):
     start_time = time.time()
     for i in range(len(arr)):
         if arr[i] == target:
-            return time.time() - start_time
-    return -1
+            elapsed_time = (time.time() - start_time) * 1000  # Convert to milliseconds
+            return i, elapsed_time
+    return -1, -1
